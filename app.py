39b28a1b8b423f1e831fe3c881dc83634bf8238c
Павел Марочкин <red-hot-shot@yandex.ru>
2025-09-05 19:04:04 +0300

    Организация проекта, роутеры, команды

--- app.py
+++ app.py
@@ -4,31 +4,25 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
+
+from handlers.user_private import user_private_router
+
+ALLOWED_UPDATES = ["message", "edited_message"]
 
 bot = Bot(token=os.getenv("TG_TOKEN"))
 
 dispatcher = Dispatcher()
 
 
-@dispatcher.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer("Start")
-
-
-@dispatcher.message()
-async def echo(message: types.Message, bot: Bot):
-    await bot.send_message(message.from_user.id, "Echo:")
-    await message.answer(message.text)
-    await message.reply(message.text)
-
+dispatcher.include_router(user_private_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dispatcher.start_polling(bot)
+    await dispatcher.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 asyncio.run(main())
