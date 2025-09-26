1b62e933f49ab68ac692cb61ed59a82dcf57bd42
Павел Марочкин <red-hot-shot@yandex.ru>
2025-09-26 12:48:09 +0300

    БД: передача сессии бд в обработчики (midleware)

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-from database.engine import create_db, drop_db
+from database.engine import create_db, drop_db, session_maker
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
 from common.bot_cmds_list import private
+
+from midlewares.db import DataBaseSession
 
 ALLOWED_UPDATES = ["message", "edited_message"]
 
@@ -46,6 +48,8 @@ async def on_shutdown(bot):
 async def main():
     dispatcher.startup.register(on_startup)  # Функция, которая срабатывает при запуске бота
     dispatcher.shutdown.register(on_shutdown)  # Функция, которая срабатывает при выключении бота
+
+    dispatcher.update.middleware(DataBaseSession(session_pool=session_maker))
 
     await bot.delete_webhook(drop_pending_updates=True)
 
