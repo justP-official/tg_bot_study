1c4d1315f9ee6586ea9a703b7735c6d4aea410bc
Павел Марочкин <red-hot-shot@yandex.ru>
2025-10-08 16:06:22 +0300

    Inline menu: Главное меню

---
+++ handlers/menu_processing.py
@@ -0,0 +1,20 @@
+from aiogram.types import InputMediaPhoto
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.orm_query import orm_get_banner
+from keyboards.inline import get_user_main_btns
+
+
+async def main_menu(session: AsyncSession, level:int, menu_name:str):
+    banner = await orm_get_banner(session, menu_name)
+
+    image = InputMediaPhoto(media=banner.image, caption=banner.description)
+
+    keyboard = get_user_main_btns(level=level)
+
+    return image, keyboard
+
+async def get_menu_content(session: AsyncSession, level:int, menu_name:str):
+    if level == 0:
+        return await main_menu(session, level, menu_name)
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -8,7 +8,9 @@ from database.orm_query import (
 )
 
 from filters.chat_types import ChatTypeFilter
-from keyboards.inline import get_callback_btns
+from keyboards.inline import get_callback_btns, MenuCallBack
+
+from menu_processing import get_menu_content
 
 
 
@@ -17,23 +19,23 @@ user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 
 @user_private_router.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer("Привет, я виртуальный помощник",
-                         reply_markup=get_callback_btns(btns={
-                             'Нажми меня': 'some_1'
-                         }))
-    
+async def start_cmd(message: types.Message, session: AsyncSession):
+    media, reply_markup = await get_menu_content(
+        session=session, 
+        level=0, 
+        menu_name="main",
+    )
 
-@user_private_router.callback_query(F.data.startswith('some_'))
-async def counter(callback: types.CallbackQuery):
-    number = int(callback.data.split('_')[-1])
+    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
-    await callback.message.edit_text(
-        text=f"Нажатий - {number}",
-        reply_markup=get_callback_btns(btns={
-                             'Нажми еще раз': f'some_{number+1}'
-                         }))
-    
 
-# Пример для видео как делать не нужно:
-# menu_level_menuName_category_page_productID
+@user_private_router.callback_query(MenuCallBack.filter())
+async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    media, reply_markup = await get_menu_content(
+        session=session,
+        level=callback_data.level,
+        menu_name=callback_data.menu_name,
+    )
+
+    await callback.message.edit_media(media=media, reply_markup=reply_markup)
+    await callback.answer()
